ddd321d0fac6be8617d45c33812d278437712915
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-02-23 17:45:40 +0300

    done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,10 @@
+# class Review < ActiveRecord::Base
+#   def game
+#     # Game.find(self.game_id) 
+#     belongs_to :game
+#   end
+# end
 class Review < ActiveRecord::Base
+    belongs_to :game
+  end
   
-end
